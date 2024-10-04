ac7a812900b171fc0889b10e33d575bdbc874da4
HachemFAt <hachemfattouh@gmail.com>
2024-10-04 12:53:06 -0400

    splitting osc and osc auto into 2 files

--- Osciloscope/osciloscope_2.py
+++ Osciloscope/osciloscope_2.py
@@ -2,17 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-data = np.loadtxt('/Users/hachemfattouh/Desktop/random files 5 the will to survive/Physics224-1/Osciloscope/osc.csv', delimiter=',', skiprows=1)
-#time = data[:,0][data[:,0] < -100.0000E-06]
-#data_1 = data[:,1][data[:,0] < -100.0000E-06]
-#data_2 = data[:,2][data[:,0] < -100.0000E-06]
-#
-#time_offset = -500.0000E-06
-#time = time - time_offset
+data = np.loadtxt('/Users/hachemfattouh/Desktop/random files 5 the will to survive/Physics224-1/Osciloscope/osc_auto.csv', delimiter=',', skiprows=413)
+time = data[:,0][data[:,0] < -100.0000E-06]
+data_1 = data[:,1][data[:,0] < -100.0000E-06]
+data_2 = data[:,2][data[:,0] < -100.0000E-06]
 
-time = data[:,0]
-data_1 = data[:,1]
-data_2 = data[:,2]
+time_offset = -500.0000E-06
+time = time - time_offset
+
 
 unc_raw = np.sqrt(20)
 unc = np.full(len(data_1), unc_raw)
@@ -35,9 +32,7 @@ def linear_fit(x, a, b):
     return a*x + b
 
 plt.scatter(time, data_1, label='Data 1')
-popt, pcov = curve_fit(exponential, time, data_1, sigma = unc) #p0=[1000000000000,10e-4, data_1[0]])
+popt, pcov = curve_fit(exponential, time, data_1, sigma = unc, p0=[1000000000000,10e-4, data_1[0]])
 plt.plot(time, exponential(time, *popt), label='Fit 1')
 plt.legend()
 plt.show()
-
-print(popt)x
