b77d062dba478019d637b1df50ea435d6b831671
HachemFAt <hachemfattouh@gmail.com>
2024-10-04 12:25:45 -0400

    nothing works and I need an adult

--- Osciloscope/osciloscope.py
+++ Osciloscope/osciloscope.py
@@ -2,12 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-data = np.loadtxt('Osciloscope/osciloscope.csv', delimiter=',', skiprows=1)
+data = np.loadtxt('/Users/hachemfattouh/Desktop/random files 5 the will to survive/Physics224-1/Osciloscope/osc.csv', delimiter=',', skiprows=1)
 
 time = data[:,0]
 data_1 = data[:,1]
 data_2 = data[:,2]
 unc = data[:,3]
+
+ms_time = time * 1e-3
 
 # Define chi squared function
 def reduced_chi_squared(x, y, y_exp, unc, params):
@@ -20,4 +22,15 @@ def reduced_chi_squared(x, y, y_exp, unc, params):
 
 # Define model
 def exponential(t, v_0, time_const):
-    return v_0**(-t/time_const)
+    return v_0*np.exp(-t/time_const)
+
+def linear_fit(x, a, b):
+    return a*x + b
+
+plt.scatter(ms_time, data_2, label='Data 1')
+popt, pcov = curve_fit(exponential, ms_time, data_2, sigma = unc)
+plt.plot(ms_time, exponential(ms_time, *popt), label='Fit 1')
+plt.legend()
+plt.show()
+
+print(popt)
