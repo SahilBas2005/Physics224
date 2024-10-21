4d381a676ad0b576b590f8426886c24a3832a716
SahilBas2005 <sahilbas2005@gmail.com>
2024-10-21 16:35:54 -0400

    Fixed data

--- Interferometer/Experiment 1/wavelength.py
+++ Interferometer/Experiment 1/wavelength.py
@@ -9,31 +9,34 @@ def reduced_chi_squared(x, y, y_exp, unc, params):
 
     return chi_squared / (len(y) - params)
 
-data = np.loadtxt('/Users/hachemfattouh/Desktop/random files 5 the will to survive/Physics224-1/Interferometer/Experiment 1/knob.csv', delimiter=',', skiprows=1)
+data = np.loadtxt('knob.csv', delimiter=',', skiprows=1)
 fringes = data[:,0]
 knob = data[:,1]
 unc = data[:,2]
 
 def f(x, λ):
     return 2*x/λ
+def ideal(x):
+    return 3.33*x
 
-popt, pcov = curve_fit(f, fringes, knob, sigma=unc, p0=[0.5])
+popt, pcov = curve_fit(f, knob, fringes, sigma=unc, p0=[0.5])
 
-plt.errorbar(fringes, knob, yerr=unc, fmt='o', label='Data')
-plt.plot(fringes, f(fringes, *popt), label='Fit')
-plt.xlabel('Number of Fringes')
-plt.ylabel('Knob Position (μm)')
+plt.errorbar(knob, fringes, yerr=unc, fmt='o', label='Data')
+plt.plot(knob, f(knob, *popt), label='Fit')
+plt.plot(knob, ideal(knob), label='Ideal')
+plt.ylabel('Number of Fringes')
+plt.xlabel('Knob Position (μm)')
 plt.legend()
 plt.show()
 
-residuals = knob - f(fringes, *popt)
-plt.scatter(fringes, residuals)
+residuals = fringes - f(knob, *popt)
+plt.scatter(knob, residuals)
 plt.axhline(0, color='black', lw=1, linestyle='--')
 plt.xlabel('Number of Fringes')
 plt.ylabel('Residuals (μm)')
 plt.show()
 
-chi2 = reduced_chi_squared(fringes, knob, f(fringes, *popt), unc, 1)
+chi2 = reduced_chi_squared(knob, fringes, f(knob, *popt), unc, 1)
 print('Reduced Chi Squared:', chi2)
 pstd = np.sqrt(np.diag(pcov))
 print(pstd)
